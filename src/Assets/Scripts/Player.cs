acf906b12f02c2515a5c42b75a7bd2a70c91c116
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-25 14:17:58 -0600

    Settled on using the manually changing velocity way to move a rigidbody character instead of using motors or applyforce methods.  Let's hope this works.

---
+++ src/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public float Speed = 5;
+        public float JumpForce = 20;
+
+        void FixedUpdate()
+        {
+            // Horizontal movement
+            rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Speed, rigidbody2D.velocity.y);
+
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+        }
+    }
+}
