9774fe3ab2bb25560d014737b3eeb9088898d9d7
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-25 15:26:08 -0600

    Fixed the wall stuck problem and added a rope bridge using exisitng rope technique.

--- src/Assets/Scripts/Player.cs
+++ src/Assets/Scripts/Player.cs
@@ -7,14 +7,24 @@ namespace Assets.Scripts
         public float Speed = 5;
         public float JumpForce = 20;
 
+        private bool _isGrounded;
+
         void FixedUpdate()
         {
+            _isGrounded = VerifyIsGrounded();
+
             // Horizontal movement
             rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Speed, rigidbody2D.velocity.y);
 
             // Jumping
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+            if (_isGrounded && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)))
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, JumpForce);
+        }
+
+        private bool VerifyIsGrounded()
+        {
+            var hit = Physics2D.Raycast(transform.position - new Vector3(0, .16f), -Vector2.up, .2f);
+            return hit.collider != null;
         }
     }
 }
