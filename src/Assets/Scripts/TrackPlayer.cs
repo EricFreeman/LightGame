4842cc0b80d698356333fe1d9b4e200bd2bfd273
Eric Freeman <eric.freeman@extendhealth.com>
2014-10-28 07:28:43 -0600

    made camera track player and updated level so rope is used to get past a gap now.

---
+++ src/Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TrackPlayer : MonoBehaviour
+    {
+        public Player Player;
+        public Vector3 Offset;
+
+        void Update()
+        {
+            if (Player == null) Player = FindObjectOfType<Player>();
+            else transform.position = Player.transform.position + Offset;
+        }
+    }
+}
