570b33ba459dbe9930deaf1bbc7bfe652348e027
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-05 17:55:12 -0700

    Added big water wheel thing you ride across on to level.

---
+++ src/Assets/Scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Spinner : MonoBehaviour
+    {
+        public float RotationSpeed;
+
+        void Update()
+        {
+            transform.Rotate(0, 0, RotationSpeed * Time.deltaTime);
+        }
+    }
+}
