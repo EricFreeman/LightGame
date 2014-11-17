4dca810b4ec9e6ad0fba2b77e4e3ebd1cd850ff1
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-17 08:04:57 -0700

    Added button that acts as switch, but will only be 'on' when some physics object is on it like player or box.

---
+++ src/Assets/Scripts/ButtonSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ButtonSwitch : MonoBehaviour
+    {
+        public List<GameObject> SwitchableObject;
+
+        private Vector3 _initial;
+        private bool _canSwitch;
+        private bool _hasSentMessage;
+
+        void Start()
+        {
+            _initial = transform.position;
+        }
+
+        void Update()
+        {
+            if (_canSwitch && _initial.y - transform.position.y < .1f)
+                transform.position = new Vector3(transform.position.x, transform.position.y - .01f, transform.position.z);
+            else if (!_canSwitch && _initial.y - transform.position.y > 0)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + .01f, transform.position.z);
+                _hasSentMessage = false;
+            }
+            else if (_canSwitch && _initial.y - transform.position.y >= .1f && !_hasSentMessage)
+            {
+                SwitchableObject.ForEach(x => x.SendMessage("Switch"));
+                _hasSentMessage = true;
+            }
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.tag != "Floor") _canSwitch = true;
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            if (col.tag != "Floor") _canSwitch = false;
+        }
+    }
+}
