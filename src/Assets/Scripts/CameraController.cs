4b13a716740491e63cf1ad997a563981ce35c3fc
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-23 22:20:49 -0700

    Added cues on where camera should move to while playing so it can zoom in, pull out, etc.

---
+++ src/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        [HideInInspector]
+        public List<Cue> Cues;
+
+        public float Speed = .5f;
+
+        void Start()
+        {
+            Cues = FindObjectsOfType<Cue>().ToList();
+        }
+
+        void Update()
+        {
+            var current = Cues.FirstOrDefault(x => x.IsActive);
+            var position = current != null ? current.Offset : Vector3.zero;
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, position, Speed * Time.deltaTime);
+        }
+    }
+}
---
+++ src/Assets/Scripts/Cue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Cue : MonoBehaviour
+    {
+        public bool IsActive;
+        public Vector3 Offset;
+
+        public void OnTriggerEnter2D(Collider2D col)
+        {
+            if(col.tag == "Player") IsActive = true;
+        }
+
+        public void OnTriggerExit2D(Collider2D col)
+        {
+            if(col.tag == "Player") IsActive = false;
+        }
+    }
+}
