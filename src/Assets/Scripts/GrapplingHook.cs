991c1e46b7f8ef7f34224e30ff38e13d09472fb9
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-26 18:12:20 -0700

    Start of work on grappling hook.

---
+++ src/Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GrapplingHook : MonoBehaviour
+    {
+        public float Length = 3;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var mousePosition = Input.mousePosition;
+                mousePosition.z = -Camera.main.transform.position.z;
+                var worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+                var hit = Physics2D.Linecast(transform.position, worldPosition, ~(1 << 8));
+                if (hit.collider != null)
+                {
+                    var line = new GameObject().AddComponent<LineRenderer>();
+                    line.SetVertexCount(2);
+                    line.SetPosition(0, transform.position);
+                    line.SetPosition(1, hit.point);
+                    line.SetWidth(0.025F, 0.025F);
+
+                    Debug.Log(transform.position);
+                    Debug.Log(hit.point);
+
+                    Debug.Log("Should be working!");
+                }
+            }
+        }
+    }
+}
