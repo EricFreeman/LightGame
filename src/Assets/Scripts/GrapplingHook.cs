1f087d0d655921d01a322aeb02acc0e8940cf7c2
Eric Freeman <eric.freeman@extendhealth.com>
2014-11-27 19:32:43 -0700

    start of allowing rope to wrap around objects.

--- src/Assets/Scripts/GrapplingHook.cs
+++ src/Assets/Scripts/GrapplingHook.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -6,9 +7,15 @@ namespace Assets.Scripts
     {
         public float Length = 3;
 
-        private bool _isGrappled;
+        private float _grappleLength;
+        private readonly List<Vector2> _points = new List<Vector2>();
         private LineRenderer _line;
         private GameObject _grapple;
+
+        // TODO: Switch to list of points
+        // TODO: Put new point when lose sight of first at second to last emelent in point list
+        // TODO: If you can see last two, then remove second to last points
+        // TODO: THE END
 
         void Start()
         {
@@ -18,13 +25,14 @@ namespace Assets.Scripts
             _line.gameObject.SetActive(false);
 
             _grapple = new GameObject("Grapple");
+            _grapple.AddComponent<BoxCollider2D>().size = new Vector2(.1f, .1f);
             _grapple.AddComponent<Rigidbody2D>();
             _grapple.rigidbody2D.isKinematic = true;
         }
 
         void Update()
         {
-            if (_isGrappled) UpdateGrapple();
+            if (_points.Count > 0) UpdateGrapple();
             else CheckForGrapple();
         }
 
@@ -41,33 +49,55 @@ namespace Assets.Scripts
                 var distance = Vector3.Distance(transform.position, hit.point);
                 if (hit.collider != null && distance <= Length)
                 {
-                    _line.SetPosition(0, transform.position);
-                    _line.SetPosition(1, hit.point);
+                    _line.SetVertexCount(2);
+                    _line.SetPosition(0, hit.point);
+                    _line.SetPosition(1, transform.position);
                     _line.gameObject.SetActive(true);
 
-                    var joint = gameObject.AddComponent<DistanceJoint2D>();
-                    joint.connectedBody = _grapple.rigidbody2D;
-                    joint.distance = distance;
+                    _points.Add(hit.point);
+                    _points.Add(transform.position);
+
+                    _grappleLength = Vector3.Distance(transform.position, hit.point);
+
+//                    var joint = gameObject.AddComponent<DistanceJoint2D>();
+//                    joint.connectedBody = _grapple.rigidbody2D;
+//                    joint.distance = distance;
+//                    joint.maxDistanceOnly = true;
 
                     _grapple.transform.position = hit.point;
-
-                    _isGrappled = true;
                 }
             }
         }
 
         private void UpdateGrapple()
         {
+            if(transform.rigidbody2D.velocity.y <= -1) transform.rigidbody2D.velocity = new Vector2(transform.rigidbody2D.velocity.x, -1);
             var hit = Physics2D.Linecast(transform.position, _grapple.transform.position, ~(1 << 8));
-            _line.SetPosition(0, transform.position);
-            _line.SetPosition(1, hit.point);
 
-            if (Input.GetMouseButtonDown(0))
+            if(Vector3.Distance(transform.position, _grapple.transform.position) > _grappleLength)
             {
-                Destroy(gameObject.GetComponent<DistanceJoint2D>());
-                _line.gameObject.SetActive(false);
+                transform.position -= (transform.position - _grapple.transform.position).normalized * (Vector3.Distance(transform.position, _grapple.transform.position) - _grappleLength);
+            }
 
-                _isGrappled = false;
+            if (hit.collider.gameObject != _grapple)
+            {
+                _points.Insert(_points.Count - 1, hit.point);
+
+                _line.SetVertexCount(_points.Count);
+                for(var i = 0; i < _points.Count; i++)
+                    _line.SetPosition(i, _points[i]);
+
+                _grapple.transform.position = hit.point;
+            }
+            else if (Input.GetMouseButtonDown(0) || hit.collider == null)
+            {
+//                Destroy(gameObject.GetComponent<DistanceJoint2D>());
+                _line.gameObject.SetActive(false);
+                _points.Clear();
+            }
+            else
+            {
+                _line.SetPosition(_points.Count - 1, transform.position);
             }
         }
     }
