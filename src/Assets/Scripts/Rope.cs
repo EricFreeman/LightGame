75e2a092443330d281a1b08fbfe0d8b53f25b241
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-04 18:39:28 -0700

    Removed useless base code and update rope to have option for both ends to be connected (eg can't move like a rope ladder where bottom and top and connected and not just hanging)

--- src/Assets/Scripts/Rope.cs
+++ src/Assets/Scripts/Rope.cs
@@ -8,16 +8,15 @@ namespace Assets.Scripts
     {
         public Vector3 StartPosition;
         public Vector3 EndPosition;
-
-        public Rigidbody2D Base;
         public GameObject RopeSection;
+        public bool BothSidesConnected;
 
         // TODO: Figure this out from gameobject and not hardcoded
         private const float Size = .12f;
 
-        private double SegmentCount
+        private int SegmentCount
         {
-            get { return Math.Round(Vector3.Distance(StartPosition, EndPosition) / Size, 0); }
+            get { return (int)Math.Round(Vector3.Distance(StartPosition, EndPosition) / Size, 0); }
         }
 
         private List<GameObject> _ropeSegments;
@@ -28,22 +27,13 @@ namespace Assets.Scripts
 
             for (var i = 0; i < SegmentCount; i++)
             {
-                var section = (GameObject) Instantiate(RopeSection);
-                if (i == 0)
-                {
-                    if (Base == null)
-                        section.rigidbody2D.isKinematic = true;
-                    else
-                        section.GetComponent<HingeJoint2D>().connectedBody = Base;
-//                        section.transform.SetParent(Base);
-                }
-                else
-                {
-                    section.GetComponent<HingeJoint2D>().connectedBody = _ropeSegments[i - 1].rigidbody2D;
-//                    section.transform.SetParent(_ropeSegments[i - 1].transform);
-                }
+                var section = (GameObject)Instantiate(RopeSection);
+
+                if (i == 0 || (BothSidesConnected && i == SegmentCount - 1)) section.rigidbody2D.isKinematic = true;
+                if(i != 0) section.GetComponent<HingeJoint2D>().connectedBody = _ropeSegments[i - 1].rigidbody2D;
 
                 section.transform.position = Vector3.Lerp(StartPosition, EndPosition, Size * i);
+                section.transform.SetParent(transform);
                 _ropeSegments.Add(section);
             }
         }
