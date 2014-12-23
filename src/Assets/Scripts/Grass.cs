805a8c900848c8d75138a61dec8e6daa8ab5f218
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-22 22:50:02 -0700

    First part of new system for laying down grass rocks shurbs, etc prodecurally instead of by hand.

---
+++ src/Assets/Scripts/Grass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+    public class Grass : MonoBehaviour
+    {
+        public float Length;
+        public List<GameObject> GameObjects;
+        public float Density;
+
+        void Start()
+        {
+            var count = Length * Density;
+            var rand = new Random();
+            
+            for (var i = 0; i < count; i++)
+            {
+                var t = GameObjects.ToList()[rand.Next(0, GameObjects.Count())];
+                var obj = (GameObject) Instantiate(t);
+                obj.transform.position = transform.position + new Vector3(i/Density, 0, 0);
+                obj.transform.parent = transform;
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + new Vector3(Length, 0, 0));
+            for (float i = 0; i < Length; i += .1f)
+                Gizmos.DrawLine(transform.position + new Vector3(i, 0, 0), transform.position + new Vector3(i, .25f, 0));
+        }
+    }
+}
