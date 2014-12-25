49547dd7f7e256eacec0705692e59d6b8bdda0c0
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-24 20:33:54 -0700

    Added new script to randomize foliage placement.

---
+++ src/Assets/Scripts/Foliage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Foliage : MonoBehaviour
+    {
+        public float MinScale = 1;
+        public float MaxScale = 1;
+
+        public float MaxHeightLower = .1f;
+
+        public GameObject Sprite;
+
+        void Start()
+        {
+            Sprite.transform.position -= new Vector3(0, Random.Range(0, MaxHeightLower), 0);
+        }
+    }
+}
