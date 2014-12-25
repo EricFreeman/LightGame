e9fc729478ff378012faa11c2ab36a9c835d04ad
Eric Freeman <eric.freeman@extendhealth.com>
2014-12-24 20:51:18 -0700

    Also randomize scale.

--- src/Assets/Scripts/Foliage.cs
+++ src/Assets/Scripts/Foliage.cs
@@ -4,7 +4,7 @@ namespace Assets.Scripts
 {
     public class Foliage : MonoBehaviour
     {
-        public float MinScale = 1;
+        public float MinScale = .3f;
         public float MaxScale = 1;
 
         public float MaxHeightLower = .1f;
@@ -14,6 +14,8 @@ namespace Assets.Scripts
         void Start()
         {
             Sprite.transform.position -= new Vector3(0, Random.Range(0, MaxHeightLower), 0);
+            var scale = Random.Range(MinScale, MaxScale);
+            transform.localScale = new Vector3(scale, scale, scale);
         }
     }
 }
